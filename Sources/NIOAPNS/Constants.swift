ad0cd3b4a4d5044ac04c0273b5a3941d2c86b0a8
Moritz Sternemann <moritz@strnmn.me>
2019-01-10 01:06:51 +0100

    Added APNS constants

---
+++ Sources/NIOAPNS/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  NIOAPNS
+//
+
+internal struct K {
+    
+    static let apnsPort: UInt16 = 443
+    static let apnsDevelopmentHost = "api.development.push.apple.com"
+    static let apnsProductionHost = "api.push.apple.com"
+    
+}
