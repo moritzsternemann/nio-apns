eb7bfaadc7e1f16fd2c7318da82dc2b8d3039d43
Moritz Sternemann <moritz@strnmn.me>
2019-01-29 01:01:07 +0100

    Implemented basic notification response object

--- Sources/NIOAPNS/APNSNotificationResponse.swift
+++ Sources/NIOAPNS/APNSNotificationResponse.swift
@@ -3,9 +3,19 @@
 //  NIOAPNS
 //
 
-public struct APNSNotificationResponse: CustomStringConvertible {
+import NIOHTTP1
 
+///
+public struct APNSNotificationResponse: CustomStringConvertible {
+    
+    ///
+    internal let status: HTTPResponseStatus
+    
+    ///
+    internal let body: String
+
+    /// See `CustomStringConvertible.description`
     public var description: String {
-        return ""
+        return "\(status): \(body)"
     }
 }
